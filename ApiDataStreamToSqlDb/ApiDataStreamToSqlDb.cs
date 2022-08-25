bb916f64610d9b641b49b7441a5a237481674573
Shane White <shane.p.white@gmail.com>
2022-08-25 15:40:49 -0500

    Got project to run

--- ApiDataStreamToSqlDb/ApiDataStreamToSqlDb.cs
+++ ApiDataStreamToSqlDb/ApiDataStreamToSqlDb.cs
@@ -6,25 +6,27 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using ApiDataStreamToSqlDb.Services;
 using System.Net.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace ApiDataStreamToSqlDb
 {
     public static class ApiDataStreamToSqlDb
     {
         [FunctionName("ApiDataStreamToSqlDb")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "https://opendata.maryland.gov/resource")] 
-            Stream stream,
-            IOpenDataPortalDatasetService openDataPortalDatasetService)
+        public static async Task<IActionResult> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "sync-data")] HttpRequest req,
+            ILogger log)
         {
-            if (stream is null)
-            {
-                return new OkObjectResult("Stream is null"); // Todo: better exception handling
-            }
+            //if (stream is null)
+            //{
+            //    return new OkObjectResult("Stream is null"); // Todo: better exception handling
+            //}
 
             HttpClient httpClient = new();
 
             await httpClient.GetStreamAsync("/9xq5-z8s2.json?account_id_mdp_field_acctid=0603043312");
+            
+            
             return new OkObjectResult("Stream retreived.");
 
             // For when the service bus queue is set up:
