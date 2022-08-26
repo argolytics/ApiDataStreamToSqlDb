50ef673e030c7e0e54fcee859c39ae196a702684
Shane White <Shane.P.White@gmail.com>
2022-08-25 21:47:27 -0500

    Added URLs, and changed class to static

--- ApiDataStreamToSqlDb/ApiDataStreamToSqlDb.cs
+++ ApiDataStreamToSqlDb/ApiDataStreamToSqlDb.cs
@@ -7,26 +7,40 @@ using Microsoft.Extensions.Logging;
 using ApiDataStreamToSqlDb.Services;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.Http.Headers;
 
 namespace ApiDataStreamToSqlDb
 {
-    public static class ApiDataStreamToSqlDb
+    public class ApiDataStreamToSqlDb
     {
+        private readonly Uri _baseUrl;
+        private readonly string _authToken;
+        public ApiDataStreamToSqlDb()
+        {
+            _baseUrl = new Uri("https://opendata.maryland.gov/");
+            _authToken = "Basic " + " {{InsertKeyHere}}";
+        }
+
         [FunctionName("ApiDataStreamToSqlDb")]
-        public static async Task<IActionResult> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "sync-data")] HttpRequest req,
+        public async Task<IActionResult> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "sync-data")] HttpRequest request,
             ILogger log)
         {
             //if (stream is null)
             //{
             //    return new OkObjectResult("Stream is null"); // Todo: better exception handling
             //}
+            //await httpClient.GetStreamAsync("https://opendata.maryland.gov/resource/9xq5-z8s2.json?account_id_mdp_field_acctid=0603043312");
 
             HttpClient httpClient = new();
+            httpClient.BaseAddress = _baseUrl;
+            
+            httpClient.DefaultRequestHeaders.Authorization =  new AuthenticationHeaderValue("Authorization", _authToken);
+            HttpResponseMessage response = await httpClient.GetAsync("resource/9xq5-z8s2.json?account_id_mdp_field_acctid=0603043312");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
 
-            await httpClient.GetStreamAsync("/9xq5-z8s2.json?account_id_mdp_field_acctid=0603043312");
-            
-            
             return new OkObjectResult("Stream retreived.");
 
             // For when the service bus queue is set up:
